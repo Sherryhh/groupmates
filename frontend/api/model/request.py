fdff60e66af2948ddf9f0d5d9df52a11873dd3c6
Jiaying <jl64@g.ucla.edu>
2022-11-12 14:02:17 -0800

    add request model

---
+++ frontend/api/model/request.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append("..")
+from index import db
+
+class IndividualRequest(db.Model):
+
+    __tablename__ = "individualRequest"
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key = True)
+    sender = db.Column(db.Integer, db.ForeignKey('student.id'))
+    receiver = db.Column(db.Integer, db.ForeignKey('student.id'))
+    status = db.Column(db.Integer)
+
+    def __init__(self, sender, receiver, status):
+        self.sender = sender
+        self.receiver = receiver
+        self.status = status
+
+class GroupRequest(db.Model):
+
+    __tablename__ = "groupRequest"
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key = True)
+    sender = db.Column(db.Integer, db.ForeignKey('student.id'))
+    receiver = db.Column(db.Integer, db.ForeignKey('group.id'))
+    status = db.Column(db.Integer)
+
+    def __init__(self, sender, receiver, status):
+        self.sender = sender
+        self.receiver = receiver
+        self.status = status
